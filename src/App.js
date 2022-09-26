d01d24e971408ee225fc63fafaf4bbc02c7ab6e6
Mhezron <mhezron699@gmail.com>
2022-09-26 18:04:10 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import StockContainer from './containers/StockContainer'
 
-function App() {
+
+const stocks = ['GOOG', 'AAPL', 'FB', 'AMZN', 'NFLX']
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div style={{ backgroundImage: "url(/currency.jpg)" }}>
+      <h1>Stock Tracker</h1>
+      <div class='stocks'>
+        {stocks.map(stock => <StockContainer key={stock} stock={stock} />)}
+      </div>
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/StockContainer.js
@@ -0,0 +1,25 @@
+import { useState,useEffect, useCallback } from "react";
+import Stock from '../components/Stock';
+import getPrice from '../services/price';
+
+
+const StockContainer = ({ stock }) =>  {
+    const [price, setPrice] = useState(0)
+    const updatePrice = useCallback(async () => {
+        const newPrice = await getPrice(stock)
+        if (price !== newPrice) {
+            return setPrice(newPrice)
+        }
+    },[stock, price]) 
+    useEffect(() => {
+        const interval = setInterval(updatePrice, 1000)
+        /**the return valueof the Effect hook is a cleanup function */
+        return () => clearInterval(interval)
+    }, [updatePrice])
+    return (
+        <Stock stock={stock} price={price} />
+    )
+}
+
+
+export default StockContainer
